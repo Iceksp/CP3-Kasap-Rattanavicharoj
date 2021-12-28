92fee084f453ba1db4934a505df56eb9827ad82f
Iceksp <70077543+Iceksp@users.noreply.github.com>
2021-12-29 01:17:34 +0700

    Add files via upload

---
+++ Exercise4.py
@@ -0,0 +1,10 @@
+FD = 81
+GB = 76
+IT = 79
+CP = 88
+print("Your Score")
+print("-----------------")
+print("Foundation English : ", FD)
+print("General Business : ", GB)
+print("Introduction to Computer Systems : ", IT)
+print("Computer Programming : ", CP)
